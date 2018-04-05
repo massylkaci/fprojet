d0b60e414ea088873dde77b2b0798b36f92413a0
Massyl KACI <37805180+massylkaci@users.noreply.github.com>
2018-04-05 20:23:28 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
+#! Bin\bash
+
 if [ -n "$1" ] 
 then 
 	mkdir $1
@@ -10,5 +12,33 @@ then
 else 
 	2>&1
 	echo " parammetre manquant "
-	exit 1
+	
+echo "      -Name:.initdev/  le but de cette application est d'implémenter un outil d’initialisation de projets.
+    
+            -Syntax:-syntax1 :/initdev <nom-project/type-de-langage > #indiquer le nom et le langage
+            
+                    -syntax2 :/initdev <nom-project/type-de-licence> #indiquer le nom et le type de la licence
+                    
+                    -syntax3 :/initdev <nom-project/type-de-langage/git > #indiquer et le nom et le langage en initialisant un depot  
+
+                    -syntax4:/initdev <nom-project/type-de-langage/type-de-licence/git > 
+                    
+         Arguments: -c: Programme en langage C
+        
+                    -cpp: Programme en langage C++
+                   
+                    -py:: Programme en langage python
+                    
+                    -latex:: Redaction en Latex
+                    
+                    -BEAMER: Presentation en BEAMER
+                    
+                    -GPL:GENERAL PUBLIC LICENCE
+                    
+                    -MIT:MIT LICENCE 
+		    
+            Author: KACI Massyl  
+	    Email: massyl2010@gmail.com"
+	    
+	    exit 1
 fi
